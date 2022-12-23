80df92abc9f31229158e6588458abf83b15d9af5
Nandini Jain <80736008+nandiniinj@users.noreply.github.com>
2022-12-23 11:33:20 +0530

    relational operators

---
+++ relational.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>relational operators</title>
+</head>
+<body>
+<?php
+$a=10;
+$b=5;
+
+echo "is greater than: ";
+echo var_dump($a>$b)."<br>";
+echo "is less than: ";
+echo var_dump($a<$b)."<br>";
+echo "is less than or equal to: ";
+echo var_dump($a<=$b)."<br>";
+echo "is greater than or equal to: ";
+echo var_dump($a>=$b)."<br>";
+echo "is equal to: ";
+echo var_dump($a==$b)."<br>";
+echo "is not equal to: ";
+echo var_dump($a!=$b)."<br>";
+
+?>
+</body>
+</html>
