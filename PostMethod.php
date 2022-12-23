5ebadb8c6f44bef01e6c7dcc19734dfde2d27e69
Nandini Jain <80736008+nandiniinj@users.noreply.github.com>
2022-12-23 11:49:45 +0530

    Add files via upload
    adding submit button

--- PostMethod.php
+++ PostMethod.php
@@ -2,13 +2,14 @@
 <head>
 <title>POST Method</title>
 </head>
-<body bgcolor=yellow>
+<body bgcolor=#ccffff>
 <?php
 echo "<br><br><br><br><br>";
 echo "<center>";
 echo "<form action='PostMethod.php' method='Post'>";
-echo "Please Enter Your name: ";
+echo "Please enter your name: ";
 echo "<input type='text' size=20 name='nm'>";
+echo "<input type='submit' name='Submit'>";
 echo "</center>";
 ?>
 </body>
