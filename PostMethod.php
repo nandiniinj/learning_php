93900879f1247235ed656ed5f03b2d22151190b4
Nandini Jain <80736008+nandiniinj@users.noreply.github.com>
2022-12-23 11:42:09 +0530

    Add files via upload
    Creating a form

---
+++ PostMethod.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>POST Method</title>
+</head>
+<body bgcolor=yellow>
+<?php
+echo "<br><br><br><br><br>";
+echo "<center>";
+echo "<form action='PostMethod.php' method='Post'>";
+echo "Please Enter Your name: ";
+echo "<input type='text' size=20 name='nm'>";
+echo "</center>";
+?>
+</body>
+</html>
