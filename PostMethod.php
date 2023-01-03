865521c0116e3bdf7be7dc83a17409a85fa5a82b
Nandini Jain <80736008+nandiniinj@users.noreply.github.com>
2023-01-03 17:22:37 +0530

    Add files via upload
    changes

--- PostMethod.php
+++ PostMethod.php
@@ -6,9 +6,13 @@
 <?php
 echo "<br><br><br><br><br>";
 echo "<center>";
-echo "<form action='PostMethod.php' method='Post'>";
+echo "<form action='userdetails.php' method='Post'>";
 echo "Please enter your name: ";
 echo "<input type='text' size=20 name='nm'>";
+echo "<br><br>";
+echo "Enter your email id: ";
+echo "<input type='text' size=20 name='mail'>";
+echo " <br><br>";
 echo "<input type='submit' name='Submit'>";
 echo "</center>";
 ?>
