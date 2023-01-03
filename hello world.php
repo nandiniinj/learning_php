a3c67dfad3adda223617291c6019fce7a1082a89
Nandini Jain <80736008+nandiniinj@users.noreply.github.com>
2023-01-03 17:26:28 +0530

    Add files via upload

---
+++ hello world.php
@@ -0,0 +1,18 @@
+<html>
+<title>my page </title>
+<head>
+<body bgcolor="yellow">
+<?php 
+echo  "hello world"."<br>";
+
+$a=67;
+$b=55;
+$sub=$a-$b;
+echo $a+$b ."<br>". $sub."<br>";
+echo var_dump($a>$b);
+echo var_dump(!$a);
+?>
+
+</head>
+</body>
+</html>
