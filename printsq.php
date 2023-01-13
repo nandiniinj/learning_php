ce9f1a38e1d0aeb387565223ceb741114eb05ac3
Nandini Jain <80736008+nandiniinj@users.noreply.github.com>
2023-01-13 18:45:10 +0530

    Add files via upload

---
+++ printsq.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title> Post Method </title>
+<head>
+<body bgcolor=lightblue>
+<?php
+echo "<br><br><br><br>";
+echo "<center>";
+$n=$_POST['num'];
+$square =$n*$n;
+echo "Square of the number is: ".$square;
+echo"</center>";
+?>
+</body>
+</html>
