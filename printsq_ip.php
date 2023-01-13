80c0091edf49ee587cda9e28f26ab63d2a160053
Nandini Jain <80736008+nandiniinj@users.noreply.github.com>
2023-01-13 18:45:28 +0530

    Add files via upload

---
+++ printsq_ip.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Square of a number</title>
+</head>
+<body bgcolor=skyblue>
+<?php
+echo "<br><br><br><br>";
+echo "<center>";
+echo "<form action ='printsq.php' method ='Post'>";
+echo "Please enter a number:";
+echo "<input type='text' size=20 name='num'>";
+echo "<input type='Submit' name ='submit'>";
+echo "</center>";
+?>
+</body>
+</html>
