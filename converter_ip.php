75003005c1f7865dcc0e438aaca3e8144756f9e6
Nandini Jain <80736008+nandiniinj@users.noreply.github.com>
2023-01-13 18:46:29 +0530

    Add files via upload

---
+++ converter_ip.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Currency converter</title>
+<body bgcolor=lightyellow>
+<?php
+echo "<br><br><br><br><br><br><br><br><br>";
+echo "<center>";
+echo "<form action='converter.php' method='Post'>";
+echo "Please enter the amount in Rupees: ";
+echo "<input type='text' size=20 name='amt'>";
+echo "<input type='Submit' name='submit'>";
+echo "</center>";
+?>
+</body>
+</html>
